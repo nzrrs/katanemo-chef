2c13b97ea89de62442b8aa66badac2146d6679f4
Nizar Sadiki <164057221+nzrrs@users.noreply.github.com>
2025-10-27 22:12:31 +0100

    Update logo and header text in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import logo from '../assets/images/chef-claude-icon.png';
+import logo from '../assets/images/katanemo-logo.png';
 
 export default function Header(){
     return(
         <header>
-            <img src={logo} alt="claude-chef-logo" />
-            <h1>Chef Claude</h1>
+            <img src={logo} alt="katanemo-logo" />
+            <h1>Katanemo Chef</h1>
         </header>
     )
 }
